a68ddba52b818536210d08cead0f4dbd4c4cbd81
Dan Shrout <danshrout@yahoo.com>
2018-10-19 11:27:28 -0700

    create new classes for restaurants and recipes.

--- app/src/main/java/com/danshrout/thefoodapp/MainActivity.java
+++ app/src/main/java/com/danshrout/thefoodapp/MainActivity.java
@@ -1,13 +1,38 @@
 package com.danshrout.thefoodapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button recipesButton;
+    private Button restaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recipesButton = findViewById(R.id.recipeButton);
+        restaurantsButton = findViewById(R.id.restaurantButton);
+
+        recipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent recipeIntent = new Intent(MainActivity.this,RecipeActivity.class);
+                startActivity(recipeIntent);
+            }
+        });
+
+        restaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent restaurantIntent = new Intent(MainActivity.this,RestaurantActivity.class);
+                startActivity(restaurantIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/danshrout/thefoodapp/RecipeActivity.java
@@ -0,0 +1,16 @@
+package com.danshrout.thefoodapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RecipeActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+
+    }
+}
