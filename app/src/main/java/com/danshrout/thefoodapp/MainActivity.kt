1cb0c1d2e82e85ace9ef1488a8bc22d327b46e7b
Dan Shrout <danshrout@yahoo.com>
2021-01-25 10:51:27 -0800

    refactored code and is now ready to add new features.

--- app/src/main/java/com/danshrout/thefoodapp/MainActivity.kt
+++ app/src/main/java/com/danshrout/thefoodapp/MainActivity.kt
@@ -1,51 +1,12 @@
 package com.danshrout.thefoodapp
 
-import android.content.Intent
 import android.os.Bundle
-<<<<<<< HEAD
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-=======
-import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-
-class MainActivity : AppCompatActivity() {
-
-
->>>>>>> 37bab9ec2ab18d3511da658a32eab85a8692b364
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-<<<<<<< HEAD
-        //Buttons
-        val recipes_button = findViewById<Button>(R.id.recipes_button)
-        val restaurants_button = findViewById<Button>(R.id.restaurants_button)
-
-        //Navigate to recipes page
-=======
-
->>>>>>> 37bab9ec2ab18d3511da658a32eab85a8692b364
-        recipes_button.setOnClickListener {
-            val intent = Intent(this, RecipeActivity::class.java)
-            startActivity(intent)
-        }
-
-<<<<<<< HEAD
-        //TODO Build out restaurant features after MVP is complete.
-        //Navigate to restaurant page
-//        restaurants_button.setOnClickListener {
-//            val intent = Intent( this, RestaurantActivity::class.java)
-//            startActivity(intent)
-//        }
-=======
-        restaurants_button.setOnClickListener {
-            val intent = Intent( this, RestaurantActivity::class.java)
-            startActivity(intent)
-        }
->>>>>>> 37bab9ec2ab18d3511da658a32eab85a8692b364
     }
 }
